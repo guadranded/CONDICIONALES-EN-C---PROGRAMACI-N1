8a4a64c9c275a716b989b6c7dd0460853bbbd634
guadranded <160340065+guadranded@users.noreply.github.com>
2024-02-23 22:41:42 -0600

    Update Ejemplo del uso de else.cpp

--- Ejemplo del uso de else.cpp
+++ Ejemplo del uso de else.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(){
-	//solicitar al usuario que ingrese un número
-	cout << "Ingrese un número: ";
+	cout << "Ingrese un nÃºmero: ";
 	int numero;
 	cin >> numero;
-	
-	//verificar si el número es positivo, negativo o cero
+	//uso de la condicional if para verificar si el nÃºmero es positivo, negativo o cero.
 	if (numero > 0){
-		cout << "El número es positivo" <<endl;
+		cout << "El nÃºmero es positivo" <<endl;
 		} 
 		else if (numero < 0){
-			cout << "El número es negativo" <<endl;
+			cout << "El nÃºmero es negativo" <<endl;
 			}
 			else {
-				cout << "El número es cero" << endl;
+				cout << "El nÃºmero es cero" << endl;
 			}
 	return 0;
 }
