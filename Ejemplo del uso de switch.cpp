630efde4cf20a050f85502dfe940e43007a4cdda
guadranded <160340065+guadranded@users.noreply.github.com>
2024-02-23 22:54:48 -0600

    Add files via upload

---
+++ Ejemplo del uso de switch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main (){
+	cout << "Ingrese un número del 1 al 7 para representar un día de la semana";
+	int diasemana;
+	cin >> diasemana;
+	// uso de la condicional swicth para que muestre el día según el día elegido por el usuario
+	switch (diasemana){
+		case 1:
+			cout << "Lunes" <<endl;
+			break;
+		case 2:
+			cout << "Martes" <<endl;
+			break;
+		case 3:
+			cout << "Miércoles" <<endl;
+			break;
+		case 4:
+			cout << "Jueves" <<endl;
+			break;
+		case 5:
+			cout << "Viernes" <<endl;
+			break;
+		case 6:
+			cout << "Sábado" <<endl;
+			break;
+		case 7:
+			cout "Domingo" <<endl;
+			break;
+	}
+	return 0;
+}
